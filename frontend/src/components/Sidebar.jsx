e8b355ac5c14cc1660e6a545c2f6758da33576b6
Vinoth-46 <vinothuser7@gmail.com>
2025-12-09 12:30:03 +0530

    Error fixes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -53,7 +53,7 @@ const Sidebar = () => {
             <div className="border-b border-base-300 w-full p-5">
                 <div className="flex items-center gap-2">
                     <Users className="size-6" />
-                    <span className="font-medium block">Contacts</span>
+                    <span className="font-medium block">Friends</span>
                 </div>
                 {/* Todo: Online filter toggle */}
                 <div className="mt-3 hidden lg:flex items-center gap-2">
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
 
     return (
         <div className="h-[100dvh] bg-base-200">
-            <div className="flex items-center justify-center pt-0 px-0 sm:pt-20 sm:px-4 h-full">
+            <div className="flex items-center justify-center pt-16 px-0 sm:pt-20 sm:px-4 h-full">
                 <div className="bg-base-100 rounded-none sm:rounded-lg shadow-cl w-full max-w-6xl h-full sm:h-[calc(100vh-8rem)]">
                     <div className="flex h-full rounded-lg overflow-hidden">
                         <div className={`h-full w-full lg:w-72 ${selectedUser ? "hidden lg:flex" : "flex"}`}>
